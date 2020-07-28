91f4e23d7953d1f06d64a6475dd042ad9f74846d
r0b0x-0 <r0b0x@protonmail.ch>
2020-07-28 23:35:04 +0100

    Add Intro to Programming, The Basics, Exercises.

---
+++ intro_to_programming/the_basics/exercise_3.rb
@@ -0,0 +1,11 @@
+movies = {
+  "Video Drome": 1983,
+  "Secretary": 2002,
+  "Eternal Sunshine of the Spotless Mind": 2004,
+  "Edge of Tomorrow": 2014
+}
+
+puts movies[:"Video Drome"]
+puts movies[:"Secretary"]
+puts movies[:"Eternal Sunshine of the Spotless Mind"]
+puts movies[:"Edge of Tomorrow"]
